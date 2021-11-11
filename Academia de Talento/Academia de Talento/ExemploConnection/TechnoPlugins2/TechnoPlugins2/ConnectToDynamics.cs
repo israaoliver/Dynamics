9b3851c8628c28e2880db3fe2d0e629800fbf961
israaoliver <isrraaoliveira@gmail.com>
2021-11-11 16:45:49 -0300

    Connection add

---
+++ Academia de Talento/Academia de Talento/ExemploConnection/TechnoPlugins2/TechnoPlugins2/ConnectToDynamics.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnoPlugins2.Infrastructure;
+
+namespace TechnoPlugins2
+{
+    public class ConnectToDynamics : PluginImplement
+    {
+        public override void ExecutePlugin(IServiceProvider serviceProvider)
+        {
+            IOrganizationService service = ConnectionFactory.GetCrmService();
+
+            if (service != null)
+                throw new InvalidPluginExecutionException("CONNECTED");
+            else
+                throw new InvalidPluginExecutionException("ERROR");
+        }
+    }
+}
